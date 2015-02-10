d68f84a80845dc4aff84e85e2dce6fd0f783e992
Justin Stewart <scubastew@csu.fullerton.edu>
2015-02-09 22:25:20 -0800

    Adjust array size to 10000, add type casting of float

--- lab2.c
+++ lab2.c
@@ -6,22 +6,33 @@
 #include <stdio.h>
 
 //avg - Calculate and return average
-float avg(float sum, int count);
+float avg(int sum, int count);
 
+/*******************************************************
+ * ABOVE AVERAGE AVERAGE CALCULATOR
+ *______________________________________________________
+ * This program calculates and outputs the average of 
+ * those that are above average.
+ *______________________________________________________
+ *INPUTS: 
+ *   input: Integer input from buffer
+ *OUTPUTS:
+ *   aboveAverage: Average value calculated from input
+ ******************************************************/
 int main(void)
 {
-  int arr[100];       //Integer array
+  int arr[10000];     //Integer array
   int count;          //Counter of instances
   int aboveCount;     //Counter of above average instances
   int input;          //Input from user/file
-  float tempSum;      //Temporary sum of array
+  int tempSum;        //Temporary sum of array
   float average;      //Average of array
   float aboveAverage; //Average of above average
 
   //Initialization
   count = 0;
   aboveCount = 0;
-  tempSum = 0.0;
+  tempSum = 0;
   average = 0.0;
   aboveAverage = 0.0;
 
@@ -42,7 +53,7 @@ int main(void)
 
   //Calculate average and re-initialize tempSum
   average = avg(tempSum, count);
-  tempSum = 0.0;
+  tempSum = 0;
   
   //FOR - Loop through array adding up sum
   for(int j = 0; j < count; j++) {
@@ -57,13 +68,13 @@ int main(void)
   aboveAverage = avg(tempSum, aboveCount);
 
   //Output
- printf("%s %3f\n", "Above Average: ", aboveAverage);
+ printf("%s %3f\n", "Above Average Average: ", aboveAverage);
 
  return 0;
 
 }
 
 //avg - Calculate and return average
-float avg(float sum, int count) {
-  return sum/count;
+float avg(int sum, int count) {
+  return (float) sum/count;
 }
